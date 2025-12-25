2b4ab0fd44488f896d8e3d896d9db7141c5e4db2
Hasnain Ali <hasnain5f7@gmail.com>
2025-12-25 15:08:53 +0500

    Enhance route security: add authentication and admin middleware to user and category routes; update news links in views for proper navigation

--- routes/adminRoutes/router.js
+++ routes/adminRoutes/router.js
@@ -4,13 +4,17 @@ import userRouter from './user.routes.js';
 import categoryRouter from './category.routes.js';
 import newsRouter from './news.routes.js';
 import settingRouter from './setting.routes.js';
+import authMiddleware from "../../middlewares/auth.middleware.js";
+import adminMiddleware from "../../middlewares/admin.middleware.js";
 
 const router = Router();
 
 router.use('/', userRouter);
 
+router.use(authMiddleware);
+
 //category management routes
-router.use('/categories', categoryRouter);
+router.use('/categories', adminMiddleware, categoryRouter);
 
 //news management routes
 router.use('/news', newsRouter);
@@ -18,7 +22,7 @@ router.use('/news', newsRouter);
 //comment routes
 router.get('/comments', allComments);
 
-router.use('/settings', settingRouter);
+router.use('/settings', adminMiddleware, settingRouter);
 
 
 export default router;
--- routes/adminRoutes/user.routes.js
+++ routes/adminRoutes/user.routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { LoginPage, LoginAdmin, LogoutAdmin, dashBordPage } from '../../controllers/admin-controllers/user.controller.js';
 import { allUsers, addUserPage, addUser, updateUserPage, updateUser, deleteUser } from '../../controllers/admin-controllers/user.controller.js';
+import authMiddleware from '../../middlewares/auth.middleware.js';
+import adminMiddleware from '../../middlewares/admin.middleware.js';
 
 
 const router = express.Router();
@@ -9,14 +11,15 @@ const router = express.Router();
 router.get('/', LoginPage);
 router.post('/login', LoginAdmin);
 router.get('/logout', LogoutAdmin);
-router.get('/dashboard', dashBordPage);
+router.get('/dashboard', authMiddleware, dashBordPage);
 
 //user management routes
-router.get('/users', allUsers);
-router.get('/add-user', addUserPage);
-router.post('/add-user', addUser);
-router.get('/update-user/:id', updateUserPage);
-router.post('/update-user/:id', updateUser);
-router.get('/delete-user/:id', deleteUser);
+router.use(authMiddleware);
+router.get('/users', adminMiddleware, allUsers);
+router.get('/add-user', adminMiddleware, addUserPage);
+router.post('/add-user', adminMiddleware, addUser);
+router.get('/update-user/:id', adminMiddleware, updateUserPage);
+router.post('/update-user/:id', adminMiddleware, updateUser);
+router.get('/delete-user/:id', adminMiddleware, deleteUser);
 
 export default router;

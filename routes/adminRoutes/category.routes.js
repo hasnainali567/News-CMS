6d4db53c78978eed6027feffcf515f79412ba2e2
Hasnain Ali <hasnain5f7@gmail.com>
2025-12-29 15:23:21 +0500

    Enhance category and news routes: add validation middleware for category and news creation/updating

--- routes/adminRoutes/category.routes.js
+++ routes/adminRoutes/category.routes.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { allCategories, addCategoryPage, addCategory, updateCategoryPage, updateCategory, deleteCategory } from '../../controllers/admin-controllers/category.controller.js';
-
+import isValid from '../../middlewares/validation.js'
 const router = express.Router();
 
 //category management routes
 router.get('/', allCategories);
 router.get('/add-category', addCategoryPage);
-router.post('/add-category', addCategory);
+router.post('/add-category', isValid.categoryValidation, addCategory);
 router.get('/update-category/:id', updateCategoryPage);
-router.post('/update-category/:id', updateCategory);
-router.get('/delete-category/:id', deleteCategory);
+router.post('/update-category/:id', isValid.updateCategoryValidation, updateCategory);
+router.delete('/delete-category/:id', deleteCategory);
 
 export default router;
--- routes/adminRoutes/news.routes.js
+++ routes/adminRoutes/news.routes.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import { allNews, addNewsPage, addNews, updateNewsPage, updateNews, deleteNews } from '../../controllers/admin-controllers/news.controller.js';
 import upload from '../../middlewares/multer.middleware.js';
+import uploadMiddleware from '../../middlewares/upload.middleware.js';
 const router = express.Router();
+import isValid from '../../middlewares/validation.js'
 
 //news management routes
 
 router.get('/', allNews);
-router.get('/add-news',addNewsPage);
-router.post('/add-news', upload.single('image'), addNews);
+router.get('/add-news',  addNewsPage);
+router.post('/add-news', uploadMiddleware, isValid.newsValidation,addNews);
 router.get('/update-news/:id', updateNewsPage);
-router.post('/update-news/:id', upload.single('image'), updateNews);
-router.get('/delete-news/:id', deleteNews);
+router.post('/update-news/:id', uploadMiddleware, isValid.newsValidation, updateNews);
+router.delete('/delete-news/:id', deleteNews);
 export default router;

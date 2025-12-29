e96f259309630e9a9f079b1d8f70c8814442c8a1
Hasnain Ali <hasnain5f7@gmail.com>
2025-12-29 16:34:10 +0500

    Remove server start logic from index.js for future configuration

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ app.use(globalErrorHandler);
 
 //Connect to MongoDB and start the server
 connectDB().then(() => {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+    // const PORT = process.env.PORT || 3000;
+    // app.listen(PORT, () => {
+    //     console.log(`Server is running on port ${PORT}`);
+    // });
 });
